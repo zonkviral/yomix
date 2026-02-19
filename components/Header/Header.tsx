b690bb2e7ae8bcb20478d52706a9a069d37ac138
max <justmax.xex@mail.ru>
2026-02-19 08:03:28 +0100

    refactor: replace a to Link, fix logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Permanent_Marker } from "next/font/google"
 
 import Image from "next/image"
+import Link from "next/link"
 
 const permanentMarker = Permanent_Marker({
     subsets: ["latin"],
@@ -9,13 +10,21 @@ const permanentMarker = Permanent_Marker({
 
 export const Header = () => (
     <header className="bg-surface border-primary col-span-2 flex rounded-t-sm border-b-[0.5px] px-5 py-1">
-        <div className="w-[150px].5 relative mt-1 mr-10 h-10">
-            <Image src="/logo.png" fill sizes="200" alt="logo" />
-            <h1
-                className={`${permanentMarker.className} text-0 invisible text-4xl uppercase`}
-            >
-                Yomix
-            </h1>
+        <div className="w-[150px].5 mt-1 mr-10 h-10">
+            <Link href="/" className="relative">
+                <Image
+                    src="/logo.svg"
+                    fill
+                    sizes="200"
+                    alt="logo"
+                    loading="eager"
+                />
+                <h1
+                    className={`${permanentMarker.className} text-0 invisible text-4xl uppercase`}
+                >
+                    Yomix
+                </h1>
+            </Link>
         </div>
         <form className="self-center pl-3">
             <input
