abc09f6eb32be6cf6c51f6b3ce9535dfd4c11890
max <justmax.xex@mail.ru>
2026-02-21 12:21:43 +0100

    feat: create reusable manga card component and implement to PopularList

---
+++ components/MangaCard/MangaCard.tsx
@@ -0,0 +1,31 @@
+import { getCoverUrl } from "@/lib/MangaDex/getCoverUrl"
+import { Manga } from "@/lib/MangaDex/types"
+
+import { getTitle } from "@/utils/getTitle"
+
+import Image from "next/image"
+import Link from "next/link"
+
+export const MangaCard = ({ manga }: { manga: Manga }) => {
+    const coverUrl = getCoverUrl(manga, 256)
+    return (
+        <Link href={`manga/${manga.id}`}>
+            <div className="relative isolate h-48 w-32 overflow-hidden rounded-xl shadow-lg shadow-black/50 transition-all duration-200 hover:scale-105 hover:shadow-xl hover:shadow-black/70 sm:h-60 sm:w-40 md:h-72 md:w-48 lg:h-75 lg:w-50">
+                <Image
+                    className="object-cover"
+                    src={coverUrl!}
+                    alt="cover"
+                    sizes="200px"
+                    loading="eager"
+                    fill
+                />
+                <h3 className="absolute bottom-0 left-0 line-clamp-3 w-full bg-linear-to-t from-black/60 to-[#0000007d] px-2 py-1 text-[1.1rem] font-semibold text-shadow-[1px_1px_black]">
+                    {getTitle(
+                        manga.attributes.title,
+                        manga.attributes.altTitles,
+                    )}
+                </h3>
+            </div>
+        </Link>
+    )
+}
--- components/PopularList/PopularList.tsx
+++ components/PopularList/PopularList.tsx
@@ -1,50 +1,23 @@
-import { getCoverUrl } from "@/lib/MangaDex/getCoverUrl"
 import { getPopularManga } from "@/lib/MangaDex/getPopularManga"
 import { Manga } from "@/lib/MangaDex/types"
 
 import { CarouselWrapper } from "../CarouselWrapper/CarouselWrapper"
 
-import { getTitle } from "@/utils/getTitle"
-
-import Link from "next/link"
-import Image from "next/image"
+import { List } from "../List/List"
+import { MangaCard } from "../MangaCard/MangaCard"
 
 export const PopularList = async () => {
     const data = await getPopularManga()
-    const popularListRender = () => {
-        return data.data.map((manga: Manga) => {
-            const coverUrl = getCoverUrl(manga, 256)
-            return (
-                <li key={manga.id} className="p-2 first:pl-0">
-                    <Link href={`manga/${manga.id}`}>
-                        <div className="relative isolate h-48 w-32 overflow-hidden rounded-xl shadow-lg shadow-black/50 transition-all duration-200 hover:scale-105 hover:shadow-xl hover:shadow-black/70 sm:h-60 sm:w-40 md:h-72 md:w-48 lg:h-75 lg:w-50">
-                            <Image
-                                className="object-cover"
-                                src={coverUrl!}
-                                alt="cover"
-                                sizes="200px"
-                                loading="eager"
-                                fill
-                            />
-                            <h3 className="absolute bottom-0 left-0 line-clamp-3 w-full bg-linear-to-t from-black/60 to-[#0000007d] px-2 py-1 text-[1.1rem] font-semibold text-shadow-[1px_1px_black]">
-                                {getTitle(
-                                    manga.attributes.title,
-                                    manga.attributes.altTitles,
-                                )}
-                            </h3>
-                        </div>
-                    </Link>
-                </li>
-            )
-        })
-    }
     return (
         <section className="overflow-hidden">
             <h2 className="mb-3 text-4xl font-bold">Popular Now</h2>
             <CarouselWrapper>
-                <ul className="flex list-none flex-row">
-                    {popularListRender()}
-                </ul>
+                <List
+                    className="flex list-none flex-row"
+                    listClassName="p-2 first:pl-0"
+                    list={data}
+                    renderItem={(manga: Manga) => <MangaCard manga={manga} />}
+                />
             </CarouselWrapper>
         </section>
     )
