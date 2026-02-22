cad046065c03104c3b6bee1180e21f5d9855ab3c
max <justmax.xex@mail.ru>
2026-02-22 11:28:41 +0100

    fix: fix respons

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     return (
         <html lang="en" className={`${inter.variable} ${nunito.variable}`}>
             <body>
-                <div className="grid min-h-screen w-full grid-cols-1 grid-rows-[60px_1fr_60px] xl:grid-cols-[12rem_1fr] xl:grid-rows-[60px_1fr]">
+                <div className="grid min-h-screen w-full grid-cols-1 grid-rows-[60px_1fr_60px] md:grid-cols-[12rem_1fr] md:grid-rows-[60px_1fr]">
                     <Header />
                     <Nav />
                     <main className="-mt-px overflow-hidden bg-[#111319] px-7 py-8 inset-shadow-[1px_0px_6px_0px_black]">
