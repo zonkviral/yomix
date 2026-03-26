df15d8c367697ddce8a4345f69bef086404043ec
max <justmax.xex@mail.ru>
2026-03-26 01:12:14 +0100

    feat: add divider component

---
+++ components/Divider/Divider.tsx
@@ -0,0 +1,7 @@
+export const Divider = () => (
+    <div className="my-2 flex items-center text-sm text-gray-500">
+        <span className="h-px flex-1 bg-rose-600/70" />
+        <span className="px-3">или</span>
+        <span className="h-px flex-1 bg-rose-600/70" />
+    </div>
+)
