27d27cecf6fb1be219e2d554ba3e74419a59c10d
max <justmax.xex@mail.ru>
2026-03-26 01:21:56 +0100

    feat: add caching

--- lib/MangaDex/getNewestManga.ts
+++ lib/MangaDex/getNewestManga.ts
@@ -1,14 +1,19 @@
+import { unstable_cache } from "next/cache"
 import { apiFetchHandler } from "@/utils/apiFetchHandler"
 
 import { BASE_URL } from "./constants"
 
 import { Manga, MangaListResponse } from "./types"
 
-export const getNewestManga = async (): Promise<{
-    manga: Manga[]
-}> => {
+const fetchNewestManga = async (): Promise<{ manga: Manga[] }> => {
     const data = await apiFetchHandler<MangaListResponse>(
         `${BASE_URL}/manga?order[createdAt]=desc&limit=16&includes[]=cover_art&availableTranslatedLanguage[]=ru`,
     )
     return { manga: data.data }
 }
+
+export const getNewestManga = unstable_cache(
+    fetchNewestManga,
+    ["newest-manga"],
+    { revalidate: 60 * 10 },
+)
