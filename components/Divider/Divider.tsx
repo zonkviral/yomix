006ebf8307fa41df72fda9308d701e7ecfa77b6a
max <justmax.xex@mail.ru>
2026-04-05 23:56:20 +0200

    feat: add custom text

--- components/Divider/Divider.tsx
+++ components/Divider/Divider.tsx
@@ -1,7 +1,12 @@
-export const Divider = () => (
+interface DividerProps {
+    text?: string
+    color?: string
+}
+
+export const Divider = ({ text, color }: DividerProps) => (
     <div className="my-2 flex items-center text-sm text-gray-500">
-        <span className="h-px flex-1 bg-rose-600/70" />
-        <span className="px-3">или</span>
-        <span className="h-px flex-1 bg-rose-600/70" />
+        <span className={"h-px flex-1 " + (color || "bg-rose-600/70")} />
+        <span className="px-3">{text || "или"}</span>
+        <span className={"h-px flex-1 " + (color || "bg-rose-600/70")} />
     </div>
 )
